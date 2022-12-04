2fa729d7a2a96f81da3472a4e3c57b4dfbce6bf7
williamdwinnell <93562392+williamdwinnell@users.noreply.github.com>
2022-12-04 11:36:09 -0500

    Added biases and automatic initial learning_rate

--- grad_descent_neural_net.py
+++ grad_descent_neural_net.py
@@ -8,25 +8,34 @@ class NeuralNetwork:
         self.hidden_size = hidden_size
         self.output_size = output_size
 
-        self.W1 = np.random.normal(0, 0.1, (self.input_size, self.hidden_size))
-        self.W2 = np.random.normal(0, 0.1, (self.hidden_size, self.output_size))
+        self.W1 = np.random.normal(0, 0.2, (self.input_size, self.hidden_size))
+        self.W2 = np.random.normal(0, 0.2, (self.hidden_size, self.output_size))
+        self.b1 = np.random.normal(0, 0.2, (1, self.hidden_size))
+        self.b2 = np.random.normal(0, 0.2, (1, self.output_size))
+
         self.iteration = 0
 
     def forward(self, x):
         # forward propagation
-        self.z = np.dot(x, self.W1) # dot product of input and first set of weights
+        self.z = np.dot(x, self.W1) + self.b1 # dot product of input and first set of weights, with bias
         self.h = self.sigmoid(self.z) # activation function
-        self.z2 = np.dot(self.h, self.W2) # dot product of hidden layer and second set of weights
+        self.z2 = np.dot(self.h, self.W2) + self.b2 # dot product of hidden layer and second set of weights, with bias
         y_hat = self.sigmoid(self.z2) # final activation function
         return y_hat
 
     def sigmoid(self, z):
+
+        # clip z to avoid overflow errors
+        z = np.clip(z, -100, 100)
+
         # activation function
         return 1 / (1 + np.exp(-z))
-    '''def sigmoid_prime(self, z):
-        return self.sigmoid(z) * (1 - self.sigmoid(z))'''
     
     def sigmoid_prime(self, z):
+
+        # clip z to avoid overflow errors
+        z = np.clip(z, -100, 100)
+
         # derivative of sigmoid function
         return np.exp(-z) / ((1 + np.exp(-z)) ** 2)
     
@@ -36,9 +45,16 @@ class NeuralNetwork:
         d_W2 = np.dot(self.h.T, delta * self.sigmoid_prime(self.z2))
         d_W1 = np.dot(x.T, np.dot(delta * self.sigmoid_prime(self.z2), self.W2.T) * self.sigmoid_prime(self.z))
 
+        d_b2 = np.sum(delta * self.sigmoid_prime(self.z2), axis=0, keepdims=True)
+        d_b1 = np.sum(np.dot(delta * self.sigmoid_prime(self.z2), self.W2.T) * self.sigmoid_prime(self.z), axis=0, keepdims=True)
+
         # use a dynamic learning rate that decreases as the network converges
         self.W1 += learning_rate / (1 + self.iteration / 1000) * d_W1 + np.random.normal(0, 0.1, (self.input_size, self.hidden_size))
         self.W2 += learning_rate / (1 + self.iteration / 1000) * d_W2 + np.random.normal(0, 0.1, (self.hidden_size, self.output_size))
+
+        # update hidden layer bias weights
+        self.b1 += learning_rate / (1 + self.iteration / 1000) * d_b1
+        self.b2 += learning_rate / (1 + self.iteration / 1000) * d_b2
 
         # increment the iteration counter
         self.iteration += 1
@@ -69,61 +85,93 @@ class NeuralNetwork:
 
         return average_error
 
+def find_optimal_learning_rate(x, y, network, learning_rates, num_tests):
+
+    # store initial parameters
+    temp_input_size = network.input_size
+    temp_hidden_size = network.hidden_size
+    temp_output_size = network.output_size
+
+    # define the number of epochs to use for testing
+    epochs = 15
+
+    # create a dictionary to store the results
+    results = {}
+
+    # loop through the learning rates
+    for learning_rate in learning_rates:
+        # create a list to store the results for each test
+        result_list = []
+
+        # loop through the number of tests
+        for i in range(num_tests):
+
+            network.__init__(temp_input_size, temp_hidden_size, temp_output_size)
+
+            # train the network with the current learning rate
+            network.train(x, y, epochs, learning_rate)
+
+            # evaluate the network on the training data
+            y_hat = network.forward(x)
+
+            # calculate the mean squared error
+            mse = np.mean((y - y_hat) ** 2)
+
+            # append the result to the list
+            result_list.append(mse)
+
+        # calculate the average mean squared error
+        avg_mse = np.mean(result_list)
+
+        # store the result in the dictionary
+        results[learning_rate] = avg_mse
+        print(learning_rate, ": ", avg_mse)
+    # return the learning rate with the minimum average mean squared error
+    return min(results, key=results.get)
+
 ###MAIN CODE###
 
 # load the CSV file using Pandas
-df = pd.read_csv(r'.\housepricedata.csv')
+df = pd.read_csv(r'C:\Users\liamd\OneDrive\Desktop\Projects\Optimizers_From_Scratch\housepricedata.csv')
 
 # select the columns to use as input and output data
 x = df[["LotArea", "OverallQual", "OverallCond", "TotalBsmtSF", "FullBath", "HalfBath", "BedroomAbvGr", "TotRmsAbvGrd", "Fireplaces", "GarageArea"]]
 y = df[["AboveMedianPrice"]]
-y = y.to_numpy()[:1000]
-x = x.to_numpy()[:1000]
 
-#create a StandardScaler object
+# split the data into train and test
+y_train = y.to_numpy()[:1000]
+x_train = x.to_numpy()[:1000]
+y_test = y.to_numpy()[1000:1100]
+x_test = x.to_numpy()[1000:1100]
+
+# create a StandardScaler object
 scaler = StandardScaler()
 
 # fit the scaler to the input data
-scaler.fit(x)
+scaler.fit(x_train)
 
 # transform the input data using the scaler
-x = scaler.transform(x)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
 
 # create a neural network with the same number of input and output nodes as the input and output data
-nn = NeuralNetwork(x.shape[1], 4, y.shape[1])
+nn = NeuralNetwork(x_train.shape[1], 3, y_train.shape[1])
 
-#train for 3000 epochs with an initial learning rate of 2
-nn.train(x, y, 3000, 2)
+# test different learning rates to find the optimal starting learning rate
+learning_rate = find_optimal_learning_rate(x_train, y_train, nn, [1, 0.1, 0.01, 0.001], 10)
+print(learning_rate)
 
+# train for n epochs with an initial learning rate set by the find_optimal_learning_rate function
+nn.train(x_train, y_train, 2000, learning_rate)
+
+# print the final mse to see how the model performed 
+print("Mean Error: ", nn.average_error(x_test, y_test))
+
+'''
+# Example usage of predicting an input with the model, plus a comparison to the actuals
 for i in range(3):
     input_data = x[i]
     actual_output = y[i]
     predicted_output = nn.forward(input_data)
     print("Input:", input_data, "\nPredicted output:", predicted_output, "\nActual output:", actual_output,'\n')
-
-print("Mean Error: ", nn.average_error(x, y))
-
-'''
-#Here is another simpler example using a smaller dataset.
-
-# define the input and output data
-x = np.array([[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]])
-y = np.array([[0], [0], [0], [1], [1], [1], [1], [1]])
-
-# create a neural network with 2 input nodes, 4 hidden nodes, and 1 output node
-nn = NeuralNetwork(len(x[0]), 5, 1)
-
-# train the neural network for 2000 epochs with a learning rate of 0.1
-nn.train(x, y, 300, 2)
-
-print("Input: [0, 0, 0]\nPredicted output: ", nn.forward([0, 0, 0]), "\nActual output: [0]")
-print("Input: [0, 0, 1]\nPredicted output: ", nn.forward([0, 0, 1]), "\nActual output: [0]")
-print("Input: [0, 1, 0]\nPredicted output: ", nn.forward([0, 1, 0]), "\nActual output: [0]")
-print("Input: [0, 1, 1]\nPredicted output: ", nn.forward([0, 1, 1]), "\nActual output: [1]")
-print("Input: [1, 0, 0]\nPredicted output: ", nn.forward([1, 0, 0]), "\nActual output: [1]")
-print("Input: [1, 0, 1]\nPredicted output: ", nn.forward([1, 0, 1]), "\nActual output: [1]")
-print("Input: [1, 1, 0]\nPredicted output: ", nn.forward([1, 1, 0]), "\nActual output: [1]")
-print("Input: [1, 1, 1]\nPredicted output: ", nn.forward([1, 1, 1]), "\nActual output: [1]")
-
-print("Mean Error: ", nn.average_error(x, y))
 '''
